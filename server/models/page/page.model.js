f4ebab93578770c6eb021c7fd8bd962357e8bcc8
jaylen phipps <jaylenphipps1@gmail.com>
2019-05-30 11:04:17 -0400

    stillworking

--- server/models/page/page.model.js
+++ server/models/page/page.model.js
@@ -1,13 +1,30 @@
 const mongoose = require("mongoose");
-const Pageschema = require("./page.schema")
-const PageModel = mongoose.model("pagemodel",Pageschema)
-//find pages
- PageModel.findAllPagesForWebsite = (wid) => {
-     return PageModel.find({websiteid:wid});
- }
- 
- // create page
- PageModel.create(page) => {
-     return PageModel.create (page);
- }
- 
+const PageSchema = require("./page.schema");
+const PageModel = mongoose.model("PageModel", PageSchema);
+
+// Find pages for website
+PageModel.findAllPagesForWebsite = (wid) => {
+  return PageModel.find({websiteId: wid});
+}
+
+// Create page
+PageModel.createPage = (page) => {
+  return PageModel.create(page);
+}
+
+// Find page by its id
+PageModel.findPageById = (pid) => {
+  return PageModel.findById(pid);
+}
+
+// Delete page
+PageModel.deletePage = (pid) => {
+  return PageModel.deleteOne({_id: pid});
+}
+
+// Update page
+PageModel.updatePage = (page) => {
+  return PageModel.updateOne({_id: page._id}, page);
+}
+
+module.exports = PageModel;
